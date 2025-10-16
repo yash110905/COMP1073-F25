79dabcc8fbdd48444e1347074cc8d6aa859734f1
yash110905 <153516277+yash110905@users.noreply.github.com>
2025-10-16 13:40:15 -0400

    Add files via upload

---
+++ lab-2/js/page-color-changer.js
@@ -0,0 +1,13 @@
+/* 
+    File: page-color-changer.js
+    Author: Yash Patel
+    */
+// Step 1: Get references to HTML elements
+// Range sliders
+const redSlider = document.getElementById("red");
+const greenSlider = document.getElementById("green");
+const blueSlider = document.getElementById("blue");
+
+// Body element (whose background color will change)
+const body = document.querySelector("body");
+
