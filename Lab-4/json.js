56e65beb2c3b1a42a0a39d21097b8ae74fd8bb7f
yash110905 <153516277+yash110905@users.noreply.github.com>
2025-11-22 08:43:01 -0500

    Implemented populate() async function. Added JSON fetch request using Request() and fetch().

--- Lab-4/json.js
+++ Lab-4/json.js
@@ -1,3 +1,29 @@
 /* STEP 2: Reference the HEADER and SECTION elements */
 const header = document.querySelector("header");
 const section = document.querySelector("section");
+// STEP 3a: Create the asynchronous function populate()
+async function populate() {
+  // STEP 4: URL of the JSON file
+  const requestURL = "js/i-scream.json";
+
+  // STEP 5: Create new request object
+  const request = new Request(requestURL);
+
+  // STEP 6: Fetch the JSON file
+  const response = await fetch(request);
+
+  // STEP 7: Convert to JSON
+  const jsonObj = await response.json();
+
+  // STEP 8: Log JSON to console for debugging
+  console.log(jsonObj);
+
+  // STEP 9a: Call populateHeader
+  populateHeader(jsonObj);
+
+  // STEP 10a: Call showTopFlavors
+  showTopFlavors(jsonObj);
+}
+
+// STEP 3b: Call the populate() function
+populate();
