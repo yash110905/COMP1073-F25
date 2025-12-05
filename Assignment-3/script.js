fb355f55ad476538b89b05732b4ee3c13133f2ac
yash110905 <153516277+yash110905@users.noreply.github.com>
2025-12-04 22:21:24 -0500

    Add API key and select DOM elements

---
+++ Assignment-3/script.js
@@ -0,0 +1,3 @@
+const apiKey = 'live_UQo5pZ7LChWXXYirsKLHpJqn2EXBbMI9cr2FP1GRInhNcqTCVctOHnnq4HQQLQ8r';
+const btn = document.getElementById('getDogBtn');
+const container = document.getElementById('dogContainer');
