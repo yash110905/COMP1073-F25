f1f8b5bcab67278bb66050fcf5dd129183141038
yash110905 <153516277+yash110905@users.noreply.github.com>
2025-12-04 22:21:36 -0500

    Attach click event listener to 'Get Dog' button

--- Assignment-3/script.js
+++ Assignment-3/script.js
@@ -1,3 +1,6 @@
 const apiKey = 'live_UQo5pZ7LChWXXYirsKLHpJqn2EXBbMI9cr2FP1GRInhNcqTCVctOHnnq4HQQLQ8r';
 const btn = document.getElementById('getDogBtn');
 const container = document.getElementById('dogContainer');
+btn.addEventListener('click', () => {
+  // fetch and display logic here
+});
